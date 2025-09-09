d5ca2e8bba2e3c121deb3f33f8c896211a4e59d7
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-09-09 05:47:48 +0000

    corrected library name

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,5 +1,5 @@
 author_query= Book.objects.filter(author='me')
 
-library_books= Library.objects.get(name='ibrary_name', books.all())
+library_books= Library.objects.get(name='library_name', books.all())
 
 librarian=Librarian.objects.all()
