9b0b7b8e35fec3d4c06a215faa28f92676be2a71
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-09-12 04:16:31 +0000

    added views.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -17,3 +17,7 @@ class Library(models.Model):
 class Librarian(models.Model):
     name=models.CharField(max_length=100)
     library=models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+
+
+class UserProfile(models.Model):
+    role=models.Charfield(max_length=100)
