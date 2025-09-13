5889c2dc5510a8dde2fbb4d2696d0276299a5db4
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-09-13 05:07:27 +0000

    seperated admin_view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -34,7 +34,7 @@ class Signup(CreateView):
 
    
 @user_passes_test(lambda u: u.role=='Admin')
-def admin_view(request):
+def admin_view (request):
     report=UserProfile.objects.all()
     return render(request, 'admin_view.html',{'reports':reports})
 
