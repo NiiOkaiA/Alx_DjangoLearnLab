6fb3a870044a31b7f603e995ad049c32a088c321
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-09-14 11:36:40 +0000

    red

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
-from .views import list_books,LibraryDetailView,admin_view
+from .views import list_books,LibraryDetailView,admin_view,Register
 from django.contrib.auth.views import LoginView,LogoutView
 
 urlpatterns= [ path(' ', list_books, name='book_list'),
                path(' ',LibraryDetailView.as_view(), name='LibraryDetailView'),
+              # path("admin_view/",admin_view, name='ad_view'),
+
                path('accounts/', include('django.contrib.auth.urls')),
-               path('login/',LoginView.as_view(template_name='login.html')),
+               path('login/',LoginView.as_view(template_name='relationship_app/login.html')),
                path('logout/',LogoutView.as_view(template_name='logout.html')),
+               path('register/',Register.as_view(template_name='register.html'),name='register'), 
               #      ,name= 'logout'),
                path("admin_view/",admin_view, name='ad_view')
               ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -26,7 +26,7 @@ class LibraryDetailView(DetailView):
    # context= super.get_context_data(**kwargs)
 
 
-class Signup(CreateView):
+class Register(CreateView):
     form_class =UserCreationForm()
     success_url= reverse_lazy('login')
     template_name= 'relationship_app/register.html'
@@ -46,7 +46,7 @@ def admin_view(request):
 def is_admin(user):
     return user.is_authenticated and hasattr(user,"userprofile") and user.userprofile.role=='Admin'
 
-@login_required
+#@login_required
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
