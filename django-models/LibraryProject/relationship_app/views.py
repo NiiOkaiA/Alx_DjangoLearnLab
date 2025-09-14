e48475cf1bd4a8d3563d9d819cdab86d9562befb
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-09-14 11:40:07 +0000

    added @login

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -46,7 +46,7 @@ def admin_view(request):
 def is_admin(user):
     return user.is_authenticated and hasattr(user,"userprofile") and user.userprofile.role=='Admin'
 
-#@login_required
+@login_required
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
