7e54f7ccfe51d1a085edc4d371e9b310ba732048
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-09-17 23:42:19 +0000

    put CustomUser in Bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -5,3 +5,10 @@ class Book(models.Model):
     title=models.CharField(max_length=200)
     author=models.CharField(max_length=100)
     publication_year=models.IntegerField()
+
+
+def CustomUser(AbstractUser):
+    date_of_birth=models.DateField()
+    profile_photo=models.ImageField()
+
+    objects=CustomUserManager()
