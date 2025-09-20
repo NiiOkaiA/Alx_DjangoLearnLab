9b2f8df1991177cc2c48d9d6950d423be9b667a9
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-09-20 15:45:19 +0000

    added ExampleForm

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -3,6 +3,16 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 
+class ExampleForm(UserCreationForm):
+    class Meta:
+        model=CustomUser
+        fields=('date_of_birth','profile_photo')
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model=CustomUser
+        fields=('date_of_birth','profile_photo')
+'''    
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model=CustomUser
@@ -13,4 +23,4 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model=CustomUser
         fields=('date_of_birth','profile_photo')
-    
+'''
