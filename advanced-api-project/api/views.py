ef02ffc46068c9e9ba77e97b4452d82d89f5f6d3
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-10-05 19:43:20 +0000

    added from django filter

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Book
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework
 #from rest_framework.permissions import IsAuthenticatedOrReadOnly
 #from rest_framework.permissions import 
 
