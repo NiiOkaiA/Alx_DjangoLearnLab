eebe6e9c9715594dadad45b852ba19808b0fcf40
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-10-05 21:21:20 +0000

    added search_fields

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from .models import Book
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters import rest_framework
+from rest_framework import filters
 #from rest_framework.permissions import IsAuthenticatedOrReadOnly
 #from rest_framework.permissions import 
 
@@ -12,6 +13,8 @@ from django_filters import rest_framework
 class ListView(generics.ListAPIView):
     queryset=Book.objects.all()
     serializer_class=BookSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields=['title','publication_year','author']
 
 
 class DetailView(generics.RetrieveAPIView):
