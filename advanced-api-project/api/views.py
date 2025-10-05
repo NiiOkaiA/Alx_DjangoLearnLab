e030c22ddec6654012de8a9e04eefb6a276574be
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-10-05 19:33:46 +0000

    try

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Book
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+#from rest_framework.permissions import IsAuthenticatedOrReadOnly
 #from rest_framework.permissions import 
 
 
