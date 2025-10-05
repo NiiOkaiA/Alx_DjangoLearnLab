fe542a92f32a7b8bde1ace438919db93c7af5e49
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-10-05 18:58:40 +0000

    added url endpoints

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,3 +1,15 @@
 from django.urls import path
+from .models import ListView
 
 
+url_patterns=[
+    '/books/',ListView.as_view(),
+    'books/create/',CreateView.as_view(),
+    'books/delete/',DeleteView.as_view(),
+    'books/update/',UpdateView.as_view(),
+    'books/detail/',DetailView.as_view(),
+    
+               
+
+
+    ]
