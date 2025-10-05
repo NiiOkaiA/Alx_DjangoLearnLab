1ca930615d2e524d620a972bd3a6811a326dbb51
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-10-05 19:07:33 +0000

    rest.permissions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Book
 from rest_framework import generics
-from rest_framework. permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class ListView(generics.ListAPIView):
