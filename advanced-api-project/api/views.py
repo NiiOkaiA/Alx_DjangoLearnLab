cbca114ee320113ce254d9cb1e85fbc1c2566701
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-10-05 19:06:12 +0000

    added isauthenticated to views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import Book
 from rest_framework import generics
+from rest_framework. permissions import IsAuthenticated
 # Create your views here.
 
-class Listview(generics.ListView):
+class ListView(generics.ListAPIView):
     queryset=Book.objects.all()
     serializer_class=BookSerializer
 
