4c51ab90f6ce6138917a25405bd21aa4fd39ab31
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-10-05 17:23:17 +0000

    added serializers

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,4 +1,16 @@
-class AuthorSerializer(serializers.ModelSerializer):
+class AuthorSerializer(serializers.ModelSerializer)m
     class Meta:
         model=Author
         fields=['author']
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Book
+        fields=['title','publication_year','author']
+
+    def validate(self,data):
+        if data['publication_year']>2025:
+            raise serializers.ValidationError("Year must not be in the future")
+
+        return data
