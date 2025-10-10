b7eabd289cc9c082682656bd560acbdefb40624c
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-10-10 06:45:41 +0000

    added serializers for various models

---
+++ capstone/Inventory/product/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import Product
+
+
+#serialized models
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Product
+        fields=['Product_id','Product_name','Cost_price','Selling_price','Quantity','Category_Id','Supplier_id','Warehouse_id']
+
+
+class WarehouseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Warehouse
+        fields=['warehouse_id','warehouse_name','warehouse_manager']
+
+
+class WarehouseManagerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=WarehouseManager
+        fields=['Manager_id','Manager_name']
+        
+class SupplierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Supplier
+        fields=['supplier_id','supplier_name']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Category
+        fields=['category_id','category_name']
