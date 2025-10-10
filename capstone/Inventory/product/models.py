425031712749b6000aeb1bb9a6447567d40d6c30
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-10-10 05:24:59 +0000

    first commit of BE capstone project

---
+++ capstone/Inventory/product/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    category_id=models.IntegerField(primary_key=True)
+    category_name=models.CharField(max_length=50)
+
+class Supplier(models.Model):
+    supplier_id=models.IntegerField(primary_key=True)
+    supplier_name=models.CharField(max_length=50)
+
+class WarehouseManager(models.Model):
+    Manager_id=models.IntegerField(primary_key=True)
+    Manager_name=models.CharField(max_length=60)
+
+class Warehouse(models.Model):
+    warehouse_id=models.IntegerField(primary_key=True)
+    warehouse_name=models.CharField(max_length=50)
+    warehouse_manager=models.ForeignKey(WarehouseManager,on_delete=models.CASCADE,related_name='manager')
+
+class Product(models.Model):
+    Product_id=models.IntegerField(primary_key=True)
+    Product_name=models.CharField(max_length=50)
+    Cost_price=models.IntegerField()
+    Selling_price=models.IntegerField()
+    Quantity=models.IntegerField()
+    Category_Id=models.ForeignKey(Category,on_delete=models.CASCADE,related_name='category')
+    Supplier_Id=models.ForeignKey(Supplier,on_delete=models.CASCADE,related_name='supplier')
+    Warehouse_Id=models.ForeignKey(Warehouse,on_delete=models.CASCADE,related_name='warehouse',default=1)
+
+
