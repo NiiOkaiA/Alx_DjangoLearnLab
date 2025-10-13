800a7ce31a2a5efa5286589d91e103ba404dc3c8
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-10-13 03:47:16 +0000

    added profile update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import register,home,post
+from .views import register,home,post,profileup
 from django.contrib.auth.views import LoginView,LogoutView
 
 urlpatterns=[
@@ -7,4 +7,5 @@ urlpatterns=[
     path("posts/",post.as_view(),name="posts"),
     path("register/",register,name="register"),
     path("login/",LoginView.as_view(template_name='blog/login.html'),name="login"),
+    path("profile/",profileup.as_view(),name="profileupdate")
     ]
