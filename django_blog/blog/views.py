4d2fa04e30a4af279d01b274e74567036f03257d
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-10-14 11:01:19 +0000

    did blogs/

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .forms import CustomUser
 from rest_framework import generics
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 
 
 # Create your views here.
@@ -27,23 +27,53 @@ def profileup(request):
 
 
 class listblogs(generics.Listview):
-    model=Posts
-    queryset=Post.objects.all()
+    model=Post
+    template_name='blog/listblogs.html'
+    context_object_name='listblog'
 
 class showblog(generics.DetailView):
-    model=Posts
+    model=Post
+    template_name='blog/showblog.html'
+    context_object_name='showblog'
+    '''
     def get_queryset(self)
-      return Post.objects.get(title=self.kwargs['title'])
+      return Post.objects.get(title=self.kwargs['title'])'''
 
 class createblog(generics.CreateView):
-    model=Posts
-    queryset=Post.objects.all()
+    model=Post
+    fields=['title','body']
+    template_name='blog/createblog.html'
+    success_url='/posts/'
+    
+    #queryset=Post.objects.all()
 
 class updateblog(generics.UpdateView):
-    model=Posts
-    queryset=post.objects.all()
+    model=Post
+    template_name='blog/updateblog.html'
+    success_url='/posts/'
+    
+   #queryset=post.objects.all()
 
 class deleteblog(generics.DeleteView):
-    model=Posts
-    queryset=post.objects.all()
+    model=Post
+    template_name='blog/deleteblog.html'
+    success_url=reverse_lazy('posts')
     
+    #queryset=post.objects.all()
+    
+'''
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from .models import Post
+from .forms import PostForm  # Optional
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_create.html'
+    success_url = '/posts/'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Attach logged-in user
+        return super().form_valid(form)'''
