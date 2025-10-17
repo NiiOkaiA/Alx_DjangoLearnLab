10fa9d43ed7982600ba88a373485c6bb61bd39e7
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-10-17 18:22:16 +0000

    added UserCreateSerializer to serializers and querysets and permission classes to user views

---
+++ capstone/Inventory/users/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=CustomUser
+        fields=['id','email','username']
+        
+class UserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=CustomUser
+        fields=['id','email','username','password']
--- capstone/Inventory/users/views.py
+++ capstone/Inventory/users/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import CustomUser
+from .serializers import UserSerializer,UserCreateSerializer
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+
+
+class add_user(generics.CreateAPIView):
+    model=CustomUser
+    serializer_class=UserCreateSerializer
+    queryset=CustomUser.objects.all
+
+class list_users(generics.ListAPIView):
+    model=CustomUser
+    serializer_class=UserSerializer
+    queryset=CustomUser.objects.all()
+    permission_class=[IsAuthenticated]
+
+class update_user(generics.UpdateAPIView):
+    model=CustomUser
+    serializer_class=UserSerializer
+    queryset=CustomUser.objects.all
+    permission_class=[IsAuthenticated]
+
+class delete_user(generics.DestroyAPIView):
+    model=CustomUser
+    serializer_class=UserSerializer
+    queryset=CustomUser.objects.all
+    permission_class=[IsAuthenticated]
+
