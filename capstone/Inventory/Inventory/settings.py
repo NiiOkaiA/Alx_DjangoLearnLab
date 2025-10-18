501a7da604c68ebaf6c72fe94dc66cc12c03f6c0
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-10-18 09:23:55 +0000

    modified signals with product and added authentication to settings

--- capstone/Inventory/Inventory/settings.py
+++ capstone/Inventory/Inventory/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'product',
+    'users',
+    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
@@ -103,6 +106,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -121,3 +134,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL= 'users.CustomUser'
--- capstone/Inventory/product/signals.py
+++ capstone/Inventory/product/signals.py
@@ -4,12 +4,14 @@ from models import Product,itemlog
 
 
 
-class itemlog(models.Model):
-    item =models.CharField(max_length=40)
-    modification=models.CharField(max_length=30)
-    time= models.DateTimeField(auto_now_add=True)
 
 
 @receiver( post_save ,sender=Product):
 def item_log_update(sender,instance,created,**kwargs):
-    if instance.Quantity 
+    if instance.Quantity++:
+        action='items restocked'
+
+    elif instance.Quantity--:
+        action='items sold'
+
+    ChangeLog.objects.create(action=action,modification=Product.User)
